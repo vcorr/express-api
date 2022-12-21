1e14cc2a8cf401f99ccaf9d06c14eeac71e6ee8b
Vasco Correia <vasco.correia@eee.do>
2022-12-21 22:12:58 +0200

    fix(deps): removed bcrypt and added prom-client

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,4 +1,4 @@
-import { hash } from 'bcrypt';
+
 import { CreateUserDto } from '@dtos/users.dto';
 import { HttpException } from '@exceptions/HttpException';
 import { User } from '@interfaces/users.interface';
@@ -26,8 +26,7 @@ class UserService {
     const findUser: User = this.users.find(user => user.email === userData.email);
     if (findUser) throw new HttpException(409, `This email ${userData.email} already exists`);
 
-    const hashedPassword = await hash(userData.password, 10);
-    const createUserData: User = { id: this.users.length + 1, ...userData, password: hashedPassword };
+    const createUserData: User = { id: this.users.length + 1, ...userData, password: "hashedPassword" };
     this.users = [...this.users, createUserData];
 
     return createUserData;
@@ -39,9 +38,8 @@ class UserService {
     const findUser: User = this.users.find(user => user.id === userId);
     if (!findUser) throw new HttpException(409, "User doesn't exist");
 
-    const hashedPassword = await hash(userData.password, 10);
     const updateUserData: User[] = this.users.map((user: User) => {
-      if (user.id === findUser.id) user = { id: userId, ...userData, password: hashedPassword };
+      if (user.id === findUser.id) user = { id: userId, ...userData, password: "hashedPassword" };
       return user;
     });
 
